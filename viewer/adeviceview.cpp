82d831beee564137542764f90eefa055a88a96e2
alexis031182 <alexis031182@gmail.com>
2015-06-20 13:14:35 +0300

    Fix: hide action widget.

--- viewer/adeviceview.cpp
+++ viewer/adeviceview.cpp
@@ -229,10 +229,10 @@ void ADeviceView::createFilterGroupAction(int grp_i) {
 
                 _img_wdg->showPreview();
 
+                animateHideActionWidget();
+
                 QWidget *flt_wdg = _dev_ctrl->filterProperties();
                 if(flt_wdg) {
-                    animateHideActionWidget();
-
                     QDialog dlg(this);
                     dlg.setWindowTitle(name);
                     dlg.setLayout(new QVBoxLayout());
