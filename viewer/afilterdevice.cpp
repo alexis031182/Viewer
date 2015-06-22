19614c8a731d992905445dd3594e0abc95930185
alexis031182 <alexis031182@gmail.com>
2015-06-22 09:58:28 +0300

    Fix: try acquire.

--- viewer/afilterdevice.cpp
+++ viewer/afilterdevice.cpp
@@ -98,7 +98,7 @@ void AFilterDevice::stop() {
 // Run.
 // ========================================================================== //
 void AFilterDevice::run(const QImage &img) {
-    if(_semaphore.tryAcquire(1))
+    if(_semaphore.tryAcquire())
         QtConcurrent::run(this, &AFilterDevice::threadRun, img);
 }
 
